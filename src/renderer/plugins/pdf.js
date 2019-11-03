49803d1d12e7b3926e43d6855a8a48f332b548c1
Kevin <unfricht.kevin@hotmail.fr>
2019-11-03 16:07:52 +0100

    DELETE (pdf plugin) : remove unused code

--- src/renderer/plugins/pdf.js
+++ src/renderer/plugins/pdf.js
@@ -1,8 +1,7 @@
 import * as JsPDF from 'jspdf'
 import 'jspdf-autotable'
-import PDFWindow from 'electron-pdf-window'
 
-const { BrowserWindow } = require('electron').remote
+// const { BrowserWindow } = require('electron').remote
 
 const api = {
     getNewDocument: () => new JsPDF(),
@@ -10,16 +9,17 @@ const api = {
         if (undefined === doc.output)
             return
 
-        const pdfWindow = new BrowserWindow({
-            width: 1024,
-            height: 800,
-            webPreferences: {
-                plugins: true,
-                webSecurity: false,
-                nodeIntegration: true,
-                webviewTag: true
-            }
-        })
+        // Plugins is not loaded in chromium since Electron 3 ...
+        // const pdfWindow = new BrowserWindow({ 
+        //     width: 1024,
+        //     height: 800,
+        //     webPreferences: {
+        //         plugins: true,
+        //         webSecurity: false,
+        //         nodeIntegration: true,
+        //         webviewTag: true
+        //     }
+        // })
         
         doc.save()
     }
