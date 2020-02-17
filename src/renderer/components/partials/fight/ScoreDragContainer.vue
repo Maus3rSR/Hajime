d72fa3f5f4561f7c8713e2d368e7bd7371e0a303
Kevin <unfricht.kevin@hotmail.fr>
2020-02-17 17:26:50 +0100

    WIP : score drag system

--- src/renderer/components/partials/fight/ScoreDragContainer.vue
+++ src/renderer/components/partials/fight/ScoreDragContainer.vue
@@ -1,4 +1,6 @@
 <script>
+import { mapState } from 'vuex'
+
 export default {
     props: {
         scoreChoosen: {
@@ -11,8 +13,12 @@ export default {
         }
     },
     computed: {
+        ...mapState('configuration', ["FIGHT_NB_FOOL_GIVE_IPPON","FIGHT_FOOL_CODE"]),
         score_length() {
             return this.score_list.length
+        },
+        last_score_index() {
+            return this.score_length - 1
         },
         can_put() {
             return this.score_length < this.limit || this.limit === 0
@@ -20,15 +26,30 @@ export default {
     },
     methods: {
         onAdd(ev) {
-            if (ev.newIndex === this.score_length - 1)
+            if (ev.newIndex !== this.last_score_index)
+                this.score_list.splice(this.last_score_index, 0, this.score_list.splice(ev.newIndex, 1)[0]);
+
+            const last_item_added = this.score_list[this.last_score_index]
+
+            if (last_item_added.code === this.FIGHT_FOOL_CODE) {
+                this.score_list.splice(this.last_score_index)
+                this.fool_count++
+            }
+        },
+        addFool() {
+            if (this.FIGHT_NB_FOOL_GIVE_IPPON <= this.fool_count)
                 return
 
-            this.score_list.splice(this.score_length - 1, 0, this.score_list.splice(ev.newIndex, 1)[0]);
+            this.fool_count++
+
+            if (this.FIGHT_NB_FOOL_GIVE_IPPON <= this.fool_count)
+                this.$emit('on-fool-reached')
         }
     },
     data() {
         return {
-            score_list: []
+            score_list: [],
+            fool_count: 0
         }
     }
 }
