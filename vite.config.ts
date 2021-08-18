f020908d28760f2e6df2493dbe6dfbc27d04edbc
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2021-08-18 23:09:59 +0200

    🚚 variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
     root: 'src',
-    plugins: [vue()],
+    plugins: [Vue()],
 })
