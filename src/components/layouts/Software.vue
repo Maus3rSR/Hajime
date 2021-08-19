ce067dd98aa14e7b0db32fe1b76394b476287d3c
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2021-08-19 14:32:22 +0200

    💄 Hajime theme config + display logo

--- src/components/layouts/Software.vue
+++ src/components/layouts/Software.vue
@@ -7,41 +7,33 @@ export default defineComponent({
 </script>
 
 <template>
-    <main id="software" data-theme="luxury">
-        <button class="btn btn-active" role="button" aria-pressed="true">
-            neutral
-        </button>
-        <button
-            class="btn btn-primary btn-active"
-            role="button"
-            aria-pressed="true"
-        >
-            primary
-        </button>
-        <button
-            class="btn btn-secondary btn-active"
-            role="button"
-            aria-pressed="true"
-        >
-            secondary
-        </button>
-        <button
-            class="btn btn-accent btn-active"
-            role="button"
-            aria-pressed="true"
-        >
-            accent
-        </button>
-        <button
-            class="btn btn-link btn-active"
-            role="button"
-            aria-pressed="true"
-        >
-            link
-        </button>
-    </main>
+    <div id="software" data-theme="luxury" class="block overflow-x-hidden bg-gradient-to-br from-primary to-secondary">
+        <header>
+            <nav class="navbar mb-2 bg-opacity-75">
+                <div class="logo flex-1 px-2 mx-2">
+                    <router-link to="/">
+                        <img class="img img-responsive" src="/assets/img/logo.png" alt="logo" />
+                    </router-link>
+                </div>
+            </nav>
+        </header>
+    </div>
 </template>
 
 <style>
 @import 'animate.css/animate.css';
+</style>
+
+<style scoped>
+#software {
+    height: 100vh;
+}
+
+.navbar {
+    min-height: 0;
+}
+
+.logo img {
+    width: 80px;
+}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,39 @@ module.exports = {
     mode: 'jit',
     purge: ['./src/**/*.{html,vue,js,jsx,ts,tsx}'],
     darkMode: false, // or 'media' or 'class'
+    plugins: [require('daisyui')],
     theme: {
         extend: {},
     },
     variants: {
         extend: {},
     },
-    plugins: [require('daisyui')],
+    daisyui: {
+        themes: [
+            {
+                'hajime': {
+                    'primary': '#a83844',
+                    'primary-focus': '#af4c5e',
+                    'primary-content': '#f7f7f7',
+                    'secondary': '#200c25',
+                    'secondary-focus': '#32133a',
+                    'secondary-content': '#af4c5e',
+                    'accent': '#31d47d',
+                    'accent-focus': '#27ba6d',
+                    'accent-content': '#ffffff',
+                    'neutral': '#25263a',
+                    'neutral-focus': '#363856',
+                    'neutral-content': '#f7f7f7',
+                    'base-100': '#15161b',
+                    'base-200': '#25263a',
+                    'base-300': '#af4c5e',
+                    'base-content': '#f7f7f7',
+                    'info': '#06bee1',
+                    'success': '#20ba37',
+                    'warning': '#ff9900',
+                    'error': '#fa1d22',
+                },
+            },
+        ],
+    },
 }
