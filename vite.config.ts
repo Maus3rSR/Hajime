c70fa8fb9971ff7c084d32c42edf1acfd7be6b9a
Simon Babay <simon@babay.fr>
2021-10-20 21:45:55 +0200

    fix: set tsconfig.json path for vite-tsconfig-paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     root: 'src',
     envDir: '../env',
-    plugins: [vue(), tsconfigPaths()],
+    plugins: [vue(), tsconfigPaths({ projects: ['../tsconfig.json'] })],
     define: {
         '__APP_VERSION__': JSON.stringify(process.env.npm_package_version)
     }
