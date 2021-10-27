b8a5f7d9acbceab7d0d23a36b42678838dc6f6ba
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2021-10-27 16:26:08 +0200

    ➖ ➕ (plugins) : add toast, remove spinners

--- src/plugins/spinner/index.ts
+++
@@ -1,9 +0,0 @@
-import type { App, Plugin } from 'vue'
-import { CirclesToRhombusesSpinner } from 'epic-spinners'
-
-const install = (app: App) => {
-    app.component('Spinner', CirclesToRhombusesSpinner)
-    return app
-}
-
-export default { install } as Plugin
